2129a06265d5501b412632f3aef0cf01aba504c2
Yoshiyuki Ieyama <yukku0423@gmail.com>
2022-05-01 23:56:01 +0900

    setup polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,43 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { createRequire } from 'module'
+import inject from '@rollup/plugin-inject'
+import stdLibBrowser from 'node-stdlib-browser'
+const require = createRequire(import.meta.url)
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    {
+      ...inject({
+        global: [
+          require.resolve('./node_modules/node-stdlib-browser/helpers/esbuild/shim'),
+          'global'
+        ],
+        process: [
+          require.resolve('./node_modules/node-stdlib-browser/helpers/esbuild/shim'),
+          'process'
+        ],
+        Buffer: [
+          require.resolve('./node_modules/node-stdlib-browser/helpers/esbuild/shim'),
+          'Buffer'
+        ]
+      }),
+      enforce: 'post'
+    },
+  ],
+  resolve: {
+    alias: stdLibBrowser,
+  },
+  optimizeDeps: {
+    include: [
+      'buffer',
+      'process',
+    ]
+  },
+  build: {
+    target: 'es2020'
+  }
 })
